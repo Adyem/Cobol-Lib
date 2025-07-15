9482f33609dd47962aed43d01ff831415436e2d9
Bastian Vangenechten <bvangene@a1-r3-p3.s19.be>
2025-07-15 15:28:29 +0200

    adding

--- main.cob
+++ main.cob
@@ -9,6 +9,7 @@
        01  WS-LEN-01        PIC 9(3) COMP-5.
        01  WS-CMP-RET       PIC S9(1) COMP-5.
        01  WS-STRLEFT-RET   PIC X(10).
+       01  WS-STRLEFT-RET-L PIC 9(9) COMP-5.
        01  WS-STRLEFT-LEN   PIC 9(3) COMP-5.
        01  WS-STR-02        PIC X(25).
        01  WS-LEN-02        PIC 9(3) COMP-5.
@@ -53,12 +54,14 @@
            DISPLAY ""
            MOVE "Hello world" TO WS-STR-01
            MOVE LENGTH OF WS-STR-01 TO WS-LEN-01
+           MOVE LENGTH OF WS-STRLEFT-RET TO WS-STRLEFT-RET-L
            MOVE 5 TO WS-STRLEFT-LEN
            CALL 'STRLEFT' USING
                WS-STR-01
                WS-LEN-01
                WS-STRLEFT-LEN
                WS-STRLEFT-RET
+               WS-STRLEFT-RET-L
            DISPLAY "strleft string " WS-STR-01
            DISPLAY WS-STRLEFT-RET
 
