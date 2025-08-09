b58e13dd1ba17e15b455369381b7464ca64fb7d4
Adyem <136982734+Adyem@users.noreply.github.com>
2025-08-09 13:30:03 +0200

    Use structured string type across library

--- strcmp.cob
+++ strcmp.cob
@@ -9,23 +9,24 @@
 
        LINKAGE SECTION.
        01  LS-STRCMP-RETURN        PIC S9(9) COMP-5.
-       01  LS-STRCMP-SRC1          PIC X(255).
-       01  LS-STRCMP-SRC1-LENGTH   PIC 9(5) COMP-5.
-       01  LS-STRCMP-SRC2          PIC X(255).
-       01  LS-STRCMP-SRC2-LENGTH   PIC 9(5) COMP-5.
+       COPY STRING REPLACING ==MY-STRING== BY ==LS-STRCMP-SRC1==
+                     ==MY-LEN== BY ==LS-STRCMP-SRC1-LEN==
+                     ==MY-BUF== BY ==LS-STRCMP-SRC1-BUF==.
+       COPY STRING REPLACING ==MY-STRING== BY ==LS-STRCMP-SRC2==
+                     ==MY-LEN== BY ==LS-STRCMP-SRC2-LEN==
+                     ==MY-BUF== BY ==LS-STRCMP-SRC2-BUF==.
 
        PROCEDURE DIVISION USING LS-STRCMP-RETURN
-           LS-STRCMP-SRC1 LS-STRCMP-SRC2
-           LS-STRCMP-SRC1-LENGTH LS-STRCMP-SRC2-LENGTH.
+           LS-STRCMP-SRC1 LS-STRCMP-SRC2.
            MOVE 1 TO WS-INDEX
            MOVE 0 TO LS-STRCMP-RETURN
 
-           PERFORM UNTIL WS-INDEX > LS-STRCMP-SRC1-LENGTH OR
-                -    WS-INDEX > LS-STRCMP-SRC2-LENGTH 
-                IF LS-STRCMP-SRC1(WS-INDEX:1) IS NOT EQUAL TO
-                   LS-STRCMP-SRC2(WS-INDEX:1)
-                  IF LS-STRCMP-SRC1(WS-INDEX:1) IS LESS THAN
-                     LS-STRCMP-SRC2(WS-INDEX:1)
+       PERFORM UNTIL WS-INDEX > LS-STRCMP-SRC1-LEN OR
+                -    WS-INDEX > LS-STRCMP-SRC2-LEN
+               IF LS-STRCMP-SRC1-BUF(WS-INDEX:1) IS NOT EQUAL TO
+                   LS-STRCMP-SRC2-BUF(WS-INDEX:1)
+                  IF LS-STRCMP-SRC1-BUF(WS-INDEX:1) IS LESS THAN
+                     LS-STRCMP-SRC2-BUF(WS-INDEX:1)
                        MOVE -1 TO LS-STRCMP-RETURN
                   ELSE
                        MOVE 1 TO LS-STRCMP-RETURN
@@ -35,15 +36,15 @@
                ADD 1 TO WS-INDEX
                END-ADD
            END-PERFORM
-           IF LS-STRCMP-SRC1-LENGTH IS GREATER THAN
-                LS-STRCMP-SRC2-LENGTH
+       IF LS-STRCMP-SRC1-LEN IS GREATER THAN
+                LS-STRCMP-SRC2-LEN
                 MOVE 1 TO LS-STRCMP-RETURN
-           ELSE
-                IF LS-STRCMP-SRC1-LENGTH IS LESS THAN
-                    LS-STRCMP-SRC2-LENGTH
+       ELSE
+                IF LS-STRCMP-SRC1-LEN IS LESS THAN
+                    LS-STRCMP-SRC2-LEN
                     MOVE -1 TO LS-STRCMP-RETURN
                 ELSE
                     MOVE 0 TO LS-STRCMP-RETURN
                 END-IF
-           END-IF
+       END-IF
            GOBACK.
